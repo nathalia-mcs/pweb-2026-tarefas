a8b505ed864e8f1d7c877f55b3810cec41a5e8d1
Nathalia Cordeiro <nathaliamilenic@gmail.com>
2026-06-02 21:55:38 -0300

    finalizando

--- tarefa03/app/views.py
+++ tarefa03/app/views.py
@@ -3,5 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    dados_usuario = {"nome": "Michael Douglas", "idade": 23}
-    return render(request, "index.html", dados_usuario)
+    return render(request, "index.html")
+
+def usuarios(request):
+    dados_usuario = [
+    {"nome": "Michael Douglas", "matricula": "20201111110090", "idade": 23, "cidade": "São Paulo do Potengi"},
+    {"nome": "Nathalia Mileni", "matricula": "20201111110080", "idade": 18, "cidade": "São Pedro"},
+    {"nome": "Maria Cícera", "matricula": "20201111110070", "idade": 44, "cidade": "Santa Maria"},
+    {"nome": "Daniel Oliveira", "matricula": "20201111110060", "idade": 38, "cidade": "Riachuelo"},
+    {"nome": "Lara Yasmin", "matricula": "20201111110050", "idade": 27, "cidade": "Natal"},
+    ]
+    return render(request, "usuarios.html", dados_usuario)
