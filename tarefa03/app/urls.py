b9b6ffe2a229f53606b172ffc756476be6e72cc1
Nathalia Cordeiro <nathaliamilenic@gmail.com>
2026-06-02 21:57:13 -0300

    ultima modificacao

--- tarefa03/app/urls.py
+++ tarefa03/app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('/usuarios', views.usuarios, name="index"),
+    path('usuarios/', views.usuarios, name="usuarios"),
 ]
